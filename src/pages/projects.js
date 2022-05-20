9978ad761ef06af46f049ded3785eb6a469d4dbe
Ashutosh Kalambe <kalambe125@gmail.com>
2022-05-20 18:00:13 +0530

    Updated Dots

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -225,14 +225,14 @@ class services extends Component {
               <div className="dot dot-2" />
               <div className="dot dot-3" />
               <div className="dot dot-4" />
-              <div className="dot dot-5" />
-              <div className="dot dot-6" />
-              <div className="dot dot-7" />
+              {/* <div className="dot dot-5" /> */}
+              {/* <div className="dot dot-6" /> */}
+              {/* <div className="dot dot-7" /> */}
               <div className="dot dot-8" />
-              <div className="dot dot-9" />
+              {/* <div className="dot dot-9" /> */}
               <div className="dot dot-10" />
               <div className="dot dot-11" />
-              <div className="dot dot-12" />
+              {/* <div className="dot dot-12" /> */}
               <div className="dot dot-13" />
               <div className="dot dot-14" />
             </div>
